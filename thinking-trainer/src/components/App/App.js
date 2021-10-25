021e768122f26989085368b6a9f6a15addc24b69
Georgiy Gusev <gus3225@yandex.ru>
2021-10-25 20:55:12 +0500

    feat: make random machine

---
+++ thinking-trainer/src/components/App/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import quotes from '../../utils/array'
+import { useState } from 'react';
+
+const App = () => {
+  const [randomIndexQuotes, setRandomIndexQuotes] = useState([])
+
+  const handleChange = () => {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+
+    if (randomNumber === randomIndexQuotes.randomIndex) {
+      if (randomNumber === quotes.length - 1) {
+        randomNumber = randomNumber - 1;
+      }
+      randomNumber += 1;
+    }
+    setRandomIndexQuotes([quotes[randomNumber]]);
+  }
+
+  return (
+    <div className="page">
+      <div className="main">
+        <h1 className="main__quotes">{randomIndexQuotes}</h1>
+        <button className="main__button"
+          onClick={handleChange} >Центрист</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
